f1b3f5cc3bcadc9843969bba8b4f2346b30642e8
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2025-08-17 21:13:53 -0700

    remove health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,10 @@
-use std::env::var;
-use actix_web::{App, HttpServer, get, HttpResponse};
-
 mod smtp;
 
-#[get("/")]
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().body("Server is running")
-}
-
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    let bind_address = var("HOST").unwrap_or("0.0.0.0".to_string());
-    let port = var("PORT").unwrap_or("10000".to_string()).parse::<u16>().unwrap();
-
-    println!("Starting web server at {}:{}", bind_address, port);
-
+async fn main() {
     tokio::spawn(async {
         if let Err(e) = smtp::start_smtp().await {
             eprintln!("Error starting smtp server: {}", e)
         }
     });
-
-    HttpServer::new(move || {
-        App::new()
-            .service(health_check)
-    })
-    .bind(format!("{}:{}", bind_address, port))?
-    .run()
-    .await
 }
